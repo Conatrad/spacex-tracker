01b818da625ea6d37fa0ff4b3593b5ba767b21a1
Sorru <Conatrad@gmail.com>
2023-01-01 15:48:50 -0500

    Slight modifications to Card component structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter, Lancelot } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import { lutimes } from 'fs/promises'
-import Date from '../components/date';
+import Card from '../components/cards';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home({ launches }: any) {
 
  const launchArray = launches.results;
-console.log(launches)
+ 
   return (
     <>
       <Head>
@@ -20,20 +18,19 @@ console.log(launches)
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <div>
-          <h1>Upcoming Launches</h1>
-          { <ul>
+          <h1 className='m-20 text-6xl font-bold text-center'>Upcoming Launches</h1>
+          {<ul>
               {launchArray.map((launch: any) => (
-                <li className={styles.card} key={launch.id}>
-                   <h2>{launch.name}</h2>
-                    <h4>
-                      {launch.launch_service_provider.name}
-                    </h4>  
-                    <Date dateString={launch.window_start}></Date>
-                    <p className={styles.description}>
-                      Launch Description: {launch.mission.description}
-                    </p>
+                <li className="flex justify-center m-8" key={launch.id}>
+                  <Card
+                  header={launch.name}
+                  subheader={launch.launch_service_provider.name}
+                  location={launch.pad.location.name}
+                  date={launch.window_start}
+                  description="Here is a description for my card"
+                  />
                 </li>
               ))}
             </ul>}
@@ -45,22 +42,17 @@ console.log(launches)
 
 export async function getServerSideProps(){
   //Rocket Launch API v
-
   // const requestOptions = {
   //   headers: { 'Authorization': `Bearer ${process.env.API_KEY}` }
   // };
   // const res = await fetch('https://fdo.rocketlaunch.live/json/launches', requestOptions);
 
   //The Space Devs API v
-  
   const res = await fetch('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/');
   const launches = await res.json();
-
     return{
       props:{
         launches
       },
     };
-
-  
 }
